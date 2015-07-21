a18c56abe4b708067367e6eaf24c97b532a74c03
gonthalo <gonzalogalpmg@gmail.com>
2015-07-21 22:16:21 +0200

    fdgs

--- jueguecito.js
+++ jueguecito.js
@@ -1,5 +1,7 @@
 var lienzo = document.getElementByID("lienzo");
 var pluma = lienzo.getContext("2d");
+var pars[];
+var jugador;
 
 function Par() {
 	this.q = Math.floor(Math.random()*40 - 10);
@@ -11,11 +13,70 @@ function Par() {
 	this.vy = 0;
 }
 
-Par.prototype.pintar = function par_pintar(){
+Par.prototype.dibujar = function par_pintar(){
 	pluma.fillStyle = "black"
 	pluma.beginPath();
 	pluma.arc(this.x,this.y,10,0,2*Math.PI);
 	pluma.stroke();
 }
 
+Par.prototype.mover = function par_mover(dt){
+	var ax = 0;
+	var ay = 0;
+	this.x = this.x + this.vx*dt + ax*ax*dt*0.5;
+	this.y = this.y + this.vy*dt + ay*ay*dt*0.5;
+	this.vx = this.vx + ax*dt;
+	this.vy = this.vy + ay*dt;
+}
 
+function Jugador() {
+	this.q = Math.floor(Math.random()*3 - 1)*5;
+	this.m = 5;
+	this.e = 5;
+	this.x = Math.random()*1180 + 10;
+	this.y = Math.random()*480 + 10;
+	this.vx = 0;
+	this.vy = 0;
+}
+
+Jugador.prototype.dibujar = function jugador_dibujar(){
+	pluma.fillStyle = "black"
+	pluma.beginPath();
+	pluma.arc(this.x,this.y,10,0,2*Math.PI);
+	pluma.stroke();
+}
+
+Jugador.prototype.mover = function jugador_mover(dt){
+	var ax = 0;
+	var ay = 0;
+	this.x = this.x + this.vx*dt + ax*ax*dt*0.5;
+	this.y = this.y + this.vy*dt + ay*ay*dt*0.5;
+	this.vx = this.vx + ax*dt;
+	this.vy = this.vy + ay*dt;
+}
+
+function comenzar(){
+	var n;
+	for (n=0; n<0; n++){
+		
+	}
+}
+
+function dibujar(){
+	pluma.fillStyle = "white";
+	pluma.fillRect(0, 0, 1200, 500);
+}
+
+function mover() {
+}
+
+function jugar() {
+	dibujar();
+	mover();
+}
+
+window.addEventListener("keydown", function(event) {
+	jugador.entrada(event.keyCode);
+});
+
+setInterval(jugar, 25);
