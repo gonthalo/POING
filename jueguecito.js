1bbb59b982a01516023f101e8bb92d75c2686e7e
gonthalo <gonzalogalpmg@gmail.com>
2015-07-31 23:43:48 +0200

    wdfa

--- jueguecito.js
+++ jueguecito.js
@@ -1,94 +1,143 @@
+
 var lienzo = document.getElementById("lienzo");
 var pluma = lienzo.getContext("2d");
-var pars[];
-var jugador;
-
-/*function Par() {
-	this.q = Math.floor(Math.random()*40 - 10);
-	this.m = Math.floor(Math.random()*20);
-	this.e = Math.floor(Math.random()*20);
-	this.x = Math.random()*1180 + 10;
-	this.y = Math.random()*480 + 10;
-	this.vx = 0;
-	this.vy = 0;
-}
-
-Par.prototype.dibujar = function par_pintar(){
-	pluma.fillStyle = "black"
-	pluma.beginPath();
-	pluma.arc(this.x,this.y,10,0,2*Math.PI);
-	pluma.stroke();
-}
-
-Par.prototype.mover = function par_mover(dt){
-	var ax = 0;
-	var ay = 0;
-	this.x = this.x + this.vx*dt + ax*ax*dt*0.5;
-	this.y = this.y + this.vy*dt + ay*ay*dt*0.5;
-	this.vx = this.vx + ax*dt;
-	this.vy = this.vy + ay*dt;
-}
-*/
+var pars = [];
+var mapa = [];
+var g = 0;
+var magnet=0;
+var bacolor = "beige";
 function Jugador() {
-	this.q = Math.floor(Math.random()*3 - 1)*5;
-	this.m = 5;
-	this.e = 5;
-	this.x = Math.random()*1180 + 10;
-	this.y = Math.random()*480 + 10;
-	this.vx = 0;
-	this.vy = 0;
+	this.x = Math.random()*1160 + 20;
+	this.y = Math.random()*460 + 20;
+	this.vx = Math.random()*6 - 3;
+	this.vy = Math.random()*6 - 3;
 }
 
-Jugador.prototype.dibujar = function jugador_dibujar(){
+Jugador.prototype.mover = function() {
+	this.x = this.x + this.vx;
+	this.y = this.y + this.vy;
+	if (this.x>1180 || this.x<20){
+		this.vx = 0-this.vx;
+	}
+	if (this.y>480 || this.y<20){
+		this.vy = 0-this.vy;
+	}
+	if (this.y<480){
+		this.vy = this.vy + g;
+	}
+	this.vy = this.vy + 0.1*magnet*this.vx;
+	this.vx = this.vx - 0.1*magnet*this.vy;
+};
+
+Jugador.prototype.entrada = function jugador_entrada(keyCode) {
+	if(keyCode == 87 && this.vy > -15) {
+		this.vy = this.vy - 1;
+	}
+	if(keyCode == 65 && this.vx > -15) {
+		this.vx = this.vx - 1;
+	}
+	if(keyCode == 68 && this.vx < 15) {
+		this.vx = this.vx + 1;
+	}
+	if(keyCode == 83 && this.vy < 15) {
+		this.vy = this.vy + 1;
+	}
+	if(keyCode == 69){
+		this.vy = 0;
+		this.vx = 0;
+	}
+	if(keyCode == 67){
+		this.y = 250;
+		this.x = 600;
+	}
+	if (keyCode == 71) {
+		g = 1 - g;
+	}
+	if (keyCode == 82) {
+		magnet = 1;
+		bacolor = "pink";
+	}
+	if (keyCode == 76) {
+		magnet = -1;
+		bacolor = "LightBlue";
+	}
+	if (keyCode == 79) {
+		magnet = 0;
+		bacolor = "beige";
+	}
+};
+
+Jugador.prototype.dibujar = function () {
+	pluma.fillStyle = bacolor;
+	pluma.fillRect(0, 0, 1200, 500);
 	pluma.fillStyle = "black";
+	var i;
 	pluma.beginPath();
-	pluma.arc(this.x,this.y,10,0,2*Math.PI);
+	for (i=1; i<10; ++i){
+		pluma.arc(this.x,this.y,i,0,2*Math.PI);
+	}
 	pluma.stroke();
 };
 
-Jugador.prototype.mover = function jugador_mover(dt){
-	var ax = 0;
-	var ay = 0;
-	this.x = this.x + this.vx*dt + ax*ax*dt*0.5;
-	this.y = this.y + this.vy*dt + ay*ay*dt*0.5;
-	this.vx = this.vx + ax*dt;
-	this.vy = this.vy + ay*dt;
+function Par() {
+	this.x = Math.random()*1160 + 20;
+	this.y = Math.random()*460 + 20;
+	this.vx = Math.random()*6 - 3;
+	this.vy = Math.random()*6 - 3;
+}
+
+Par.prototype.mover = function() {
+	this.x = this.x + this.vx;
+	this.y = this.y + this.vy;
+	if (this.x>1180 || this.x<20){
+		this.vx = 0-this.vx;
+	}
+	if (this.y>480 || this.y<20){
+		this.vy = 0-this.vy;
+	}
+	if (this.y<480){
+		this.vy = this.vy + g;
+	}
+	this.vy = this.vy + 0.1*magnet*this.vx;
+	this.vx = this.vx - 0.1*magnet*this.vy;
 };
 
-function comenzar(){
-	/*var n;
-	for (n = 0; n < 0; ++n){
+Par.prototype.dibujar = function () {
+	pluma.fillStyle = "green";
+	var i;
+	pluma.beginPath();
+	for (i=1; i<10; ++i){
+		pluma.arc(this.x,this.y,i,0,2*Math.PI);
+	}
+	pluma.stroke();
+};
+
+function comenzar() {
+	var n;
+	for (n=0; n<1500; ++n){
+		mapa [n] = Math.floor(Math.random()*8);
+	}
+	for (n=0; n<3; ++n){
 		pars[n] = new Par();
-	}*/
+	}
 	jugador = new Jugador();
 }
 
-function dibujar(){
-	pluma.fillStyle = "white";
-	pluma.fillRect(0, 0, 1200, 500);
-	jugador.dibujar();
-	var cox = Math.random()*1100;
-	var coy = Math.random()*500;
-	pluma.fillStyle = "red";
-	pluma.fillRect(cox, coy, 20, 20);
-}
-
-function mover() {
-	var deltat = 40;
-	/*pars.forEach(function (par){
-		par.mover(dt);
-	});*/
-	jugador.mover(deltat);
-}
-
 function jugar() {
-	dibujar();
-	mover();
+	jugador.dibujar();
+	pars.forEach(function (par) {
+		par.dibujar();
+	});
+	jugador.mover();
+	pars.forEach(function (par) {
+		par.mover();
+	});
 }
 
-//window.addEventListener("keydown", function(event) {
-//	jugador.entrada(event.keyCode);
-//});
+window.addEventListener("keydown", function(event) {
+	jugador.entrada(event.keyCode);
+});
 
 comenzar();
-setInterval(jugar(), 25);
+
+window.setInterval(jugar, 25);
